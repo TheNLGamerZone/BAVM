83be1b0145b2907cb50596abf24ec992343214f6
Tip <tipbrink@hetbaarnschlyceum.nl>
2016-05-20 21:42:24 +0200

    random name generator gemaakt

--- src/me/nlt/bavm/BAVM.java
+++ src/me/nlt/bavm/BAVM.java
@@ -1,5 +1,7 @@
 package me.nlt.bavm;
 
+import me.nlt.bavm.league.RandomNames;
+
 public class BAVM
 {
     /**
@@ -8,6 +10,12 @@ public class BAVM
      */
     public static void main(String[] args)
     {
+        RandomNames randomNames = new RandomNames();
 
+        String BALTeamNames[] = randomNames.getBALTeamNames();
+
+        for (int i = 0; i < 20; i++) {
+            System.out.printf("%d. %s%n", i + 1,BALTeamNames[i]);
+        }
     }
 }
---
+++ src/me/nlt/bavm/league/PlayerStats.java
@@ -0,0 +1,19 @@
+package me.nlt.bavm.league;
+
+/**
+ * Created by Tip on 5/20/2016.
+ */
+public class PlayerStats {
+    public int[][] PlayerStats() {
+        /* de stats:
+        0: Afmaken
+        1: Aanval
+        2: Balbezit
+        3: Verdedigen
+        4: Conditie
+        (5: Doelman)
+         */
+
+    return null;
+    }
+}
