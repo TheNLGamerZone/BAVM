3c17b6acd20bedb6d3fa3dc625bba84cebbbc544
Tim Anema <timanema@hetbaarnschlyceum.nl>
2016-05-21 16:28:31 +0200

    Threads worden gelocked/unlocked zodat het display klaar is voordat iets anders gebeurd.

--- src/me/nlt/bavm/BAVM.java
+++ src/me/nlt/bavm/BAVM.java
@@ -1,13 +1,76 @@
 package me.nlt.bavm;
 
+import java.awt.*;
+
 public class BAVM
 {
+    private static BAVM mainInstance;
+    private Display display;
+
+    private final Object lockObject;
+
+
     /**
      * Main method
+     *
      * @param args Arguments
      */
     public static void main(String[] args)
     {
+        // Zo snel mogelijk weg van static
+        mainInstance = new BAVM();
+    }
 
+    /**
+     * BAVM constructor
+     */
+    public BAVM()
+    {
+        // Lock aanmaken
+        lockObject = new Object();
+
+        // Display aanmaken op EDT
+        EventQueue.invokeLater(() -> display = new Display(16, 58, lockObject));
+
+        // Wachten op unlock
+        this.lockThread();
+    }
+
+    /**
+     * Verder de game opstarten
+     */
+    private void lockThread()
+    {
+        System.out.println("Thread locked, aan het wachten op een unlock");
+        try
+        {
+            // Object 'locken'
+            synchronized (lockObject)
+            {
+                lockObject.wait();
+            }
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+        System.out.println("Thread ge-unlocked");
+
+        // Zorgen dat de rest laad
+        this.initGame();
+    }
+
+    /**
+     * Deze wordt aangeroepen nadat de thread ge-unlocked is zodat alles kan laden
+     */
+    private void initGame()
+    {
+        // Even testen
+        display.appendText("Hallo ik ben een string", "Ik wil kaas", "Ik ben ook een klant");
+    }
+
+    public static BAVM getMainInstance()
+    {
+        return mainInstance;
     }
 }
---
+++ src/me/nlt/bavm/Display.java
@@ -0,0 +1,104 @@
+package me.nlt.bavm;
+
+import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Display
+{
+    private int width, height;
+    public JTextArea textArea;
+
+    private Object lockObject;
+
+    /**
+     * Display constructor
+     *
+     * @param width  Width
+     * @param height Height
+     */
+    public Display(int width, int height, Object lockObject)
+    {
+        this.width = width;
+        this.height = height;
+        this.lockObject = lockObject;
+
+        this.initDisplay();
+    }
+
+    /**
+     * Hiermee wordt de display 'opgestart'
+     */
+    private void initDisplay()
+    {
+        JFrame jFrame = new JFrame();
+        JPanel jPanel = new JPanel();
+        this.textArea = new JTextArea(width, height);
+        JScrollPane scroll = new JScrollPane(textArea);
+
+        // JPanel, JTextArea en JScrollPane 'configureren'
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        this.textArea.setEditable(false);
+        jPanel.setBorder(new TitledBorder(new EtchedBorder(), "Actie venster"));
+        jPanel.add(scroll);
+
+        // Hier zorgen we dat onClose() wordt aangeroepen als het programma stopt
+        jFrame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        jFrame.addWindowListener(new WindowAdapter()
+        {
+            @Override
+            public void windowClosing(WindowEvent e)
+            {
+                // Op deze manier kunnen we data opslaan voordat het programma stopt
+                onClose();
+            }
+        });
+
+        // Wat standaard dingen opzetten
+        jFrame.setTitle("BAVM");
+        jFrame.add(jPanel);
+        jFrame.pack();
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+
+        // Object unlocken
+        this.resumeLoading();
+    }
+
+    /**
+     * Unlocked het lockObject zodat de main thread verder gaat met laden
+     */
+    private void resumeLoading()
+    {
+        // Object unlocken in een synchronized block zodat we de juiste MonitorState krijgen
+        synchronized (lockObject)
+        {
+            lockObject.notify();
+        }
+    }
+
+    /**
+     * Wordt aangeroepen als de gebruiker het scherm wegklikt zodat we dingen kunnen opslaan
+     */
+    private void onClose()
+    {
+        System.out.println("Afsluiten ..");
+        System.exit(1);
+    }
+
+    /**
+     * Hiermee kan tekst worden toegevoegd aan de JTextArea en dus worden getoond
+     *
+     * @param strings
+     */
+    public void appendText(String... strings)
+    {
+        // Door alle strings lopen en die printen in het actie venster
+        for (String string : strings)
+        {
+            textArea.append(string + "\n");
+        }
+    }
+}
