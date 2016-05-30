723f9487e132953f97247b228d5beb54214e8fdf
Tip <tipbrink@hetbaarnschlyceum.nl>
2016-05-30 22:44:16 +0200

    verder met simulateGame

--- src/me/nlt/bavm/game/Game.java
+++ src/me/nlt/bavm/game/Game.java
@@ -32,15 +32,64 @@ public class Game
         double visitorLuck = Math.random();
 
         double homeValues[] = new double[homeCoefficients.size()];
-        double visitorValues[] = new double[visitorCoefficients.size()];
+        double visitValues[] = new double[visitorCoefficients.size()];
 
         for (int i = 0; i < homeCoefficients.size(); i++)
         {
             homeValues[i] = (1.2 * home.getTeamInfo().getCoefficientValue(i) / 100) + (0.8 * homeLuck + 0.5);
-            visitorValues[i] = (1.2 * visitor.getTeamInfo().getCoefficientValue(i) / 100) + (0.8 * visitorLuck + 0.5);
+            visitValues[i] = (1.2 * visitor.getTeamInfo().getCoefficientValue(i) / 100) + (0.8 * visitorLuck + 0.5);
+        }
+
+        int time = 0;
+        int ballQuarter = 0;
+        int ballPossession = 0;
+
+        while (time < 2000)
+        {
+            int conflictResult = getConflictResult(homeValues, visitValues, ballPossession);
+
+            switch (conflictResult)
+            {
+
+            }
+
+            time++;
+
         }
 
 
         return goalResult;
+    }
+
+    public static int getConflictResult(double[] homeValues, double[] visitValues, int ballPossession) {
+        int conflictResult = 0;
+
+        double attValues[] = new double[homeValues.length];
+        double defValues[] = new double[homeValues.length];
+
+        if (ballPossession == 0)
+        {
+            attValues = homeValues;
+            defValues = visitValues;
+        } else if (ballPossession == 1)
+        {
+            attValues = visitValues;
+            defValues = homeValues;
+        } else
+        {
+            return -1;
+        }
+
+        for (int i = 0; i < attValues.length; i++) {
+            attValues[i] = attValues[i] * 10;
+            defValues[i] = defValues[i] * 10;
+        }
+
+        //nextQuarter formula, 0=afm, 1=att, 2=pos, 3=def, 4=kep, 5=cnd
+        int attackResult = (int) attValues[1] + (int) (500 * Math.random()) - (int) defValues[3] - (int) (500 * Math.random());
+
+
+
+        return conflictResult;
     }
 }
